15c1c92da5ba0e051b58b0419c60fdb3fcbbaad4
Zpiboo <93818768+Zpiboo@users.noreply.github.com>
2022-06-12 17:48:12 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,22 @@
-var exercises = document.querySelector(".category.exercises");
-var lessons = document.querySelector(".category.lessons");
+var categories = [...document.querySelectorAll(".category")];
+
+
+function lessons() {
+  alert("lessons");
+}
+
+function exercises() {
+  alert("exercises");
+}
+
+
+menu = {
+  "lessons": lessons,
+  "exercises": exercises
+}
+
+categories.forEach(category => {
+  category.addEventListener("click", () => {
+    menu[category.classList.item(1)]();
+  });
+});
